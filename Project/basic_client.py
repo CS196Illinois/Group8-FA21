33c19856bdcf3db501950c12e7da97aec62a2998
Kartik Subramanian <kartiks4@illinois.edu>
2021-09-29 19:22:06 -0500

    remove hash stuff

--- Project/basic_client.py
+++ Project/basic_client.py
@@ -1,18 +1,5 @@
-import base58
+import os
 import ipfshttpclient
-from hashlib import sha256
-
-def hash(d):
-    h = sha256()
-    h.update(d)
-    return h.hexdigest()
-
-def hash_file(fname):
-    with open(fname, 'rb') as f:
-        h = sha256()
-        for chunk in iter(lambda: f.read(4096),b''):
-            h.update(chunk)
-    return h.hexdigest()
 
 if __name__ == '__main__':
     client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001/http')
@@ -20,5 +7,7 @@ if __name__ == '__main__':
     with open('test_file', 'w') as f:
         f.write('CS 196 group 8\n')
 
-    hs = client.add('test_file')['hash']
-    assert hs == hash_file('test_file')
+    hs = client.add('test_file')['Hash']
+    os.system('rm test_file')
+
+    print(client.cat(hs))
